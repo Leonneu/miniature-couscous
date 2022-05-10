53cbca40eee5f9b6c1e9ce414d93be84c866afc4
Leonneu <52626756+Leonneu@users.noreply.github.com>
2022-05-10 12:10:34 +0200

    Remove useless Comments

--- src/Customer.java
+++ src/Customer.java
@@ -27,18 +27,13 @@ class Customer {
         while (enum_rentals.hasMoreElements()) {
             double thisAmount;
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
             thisAmount = each.amountFor();
-            // add frequent renter points
             frequentRenterPoints ++;
-            // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
                 frequentRenterPoints ++;
-            //show figures for this rental
             result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(thisAmount).append("\n");
             totalAmount += thisAmount;
         }
-        //add footer lines
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
         return result.toString();
